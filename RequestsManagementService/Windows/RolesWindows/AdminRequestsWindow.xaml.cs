a01030c63d594be0865191a892d7c4dc8d94dc59
iamskyline <1234fil1234@gmail.com>
2024-03-26 20:55:22 +0300

    Initialize commit

---
+++ RequestsManagementService/Windows/RolesWindows/AdminRequestsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RequestsManagementService.Windows.RolesWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminRequestsWindow.xaml
+    /// </summary>
+    public partial class AdminRequestsWindow : Window
+    {
+        public AdminRequestsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RequestsManagementService/Windows/RolesWindows/PerformerRequestsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RequestsManagementService.Windows.RolesWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для PerformerRequestsWindow.xaml
+    /// </summary>
+    public partial class PerformerRequestsWindow : Window
+    {
+        public PerformerRequestsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
